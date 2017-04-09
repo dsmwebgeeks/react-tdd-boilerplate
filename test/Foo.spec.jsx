0eac4dede06fc874650c07f536b34a7ecd0d3942
riojack <ayjay122@gmail.com>
2017-04-09 18:43:09 -0500

    Boilerplate babel, webpack, React, mocha, and related libraries

---
+++ test/Foo.spec.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Foo from '../src/Foo';
+
+describe('Foo', () => {
+  let element;
+
+  beforeEach('set up', () => {
+    element = shallow(<Foo />);
+  });
+
+  it('should render as a div', () => {
+    expect(element.type()).to.equal('div');
+  });
+});
